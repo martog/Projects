a7dbfd8a878d11960da1932578ba877ec143150e
martog <martin.holy.grigorov@gmail.com>
2016-12-17 21:02:51 +0200

    Update en_glossary.py

--- En_glossary_hw/en_glossary.py
+++ En_glossary_hw/en_glossary.py
@@ -4,7 +4,8 @@ from bs4 import BeautifulSoup
 import re
 import random
 
-def grab_info(url):
+def grab_info(term):
+	url = "http://pc.net/glossary/definition/" + term
 	html = BeautifulSoup(urllib.request.urlopen(url).read(), 'html.parser')
 	word = html.find('h2').getText()
 
@@ -21,17 +22,15 @@ def generate_url(alpha):
 	url = "http://pc.net/glossary/browse/" + alpha
 	html = BeautifulSoup(urllib.request.urlopen(url).read(), 'html.parser')
 	table = html.find("table", attrs={"class":"list"}).findAll('td', {'class' : 'term'})
-	print("."),
 	for el in table:
-		table[counter] = table[counter].getText()
+		table[counter] = table[counter].getText().lower()
+		table[counter] = ''.join(e for e in table[counter] if e.isalnum())
 		counter += 1
 	return table
 
-url = "http://pc.net/glossary/definition/android"
-
 words = []
 i = ord('a')
-print("working")
+print("working...")
 while i <= ord('z'):
 	words.extend(generate_url(chr(i)))
 	i+=1
@@ -39,5 +38,9 @@ while i <= ord('z'):
 random.shuffle(words)
 i = 0
 while i < 50:
+	print(i+1)
 	print(words[i])
+	result = grab_info(words[i])
+	print(result[0],result[1])
+	print("\n")
 	i+=1
